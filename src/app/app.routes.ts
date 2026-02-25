be0c9be2a8825a9a6f6037eda20e4714eaa07a5b
t00nst4r <tonyelzinga@gmail.com>
2026-02-25 16:40:32 +0100

    Initial push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import {Test} from './pages/test/test';
+import {Signup} from './pages/signup/signup';
 
 
 export const routes: Routes = [
   {
     path: '',
     component: Test
+  },
+  {
+    path: 'signup',
+    component: Signup
   }
 ];
